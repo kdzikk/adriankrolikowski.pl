94fbc452d72a374773577a239ed66ce7e503986f
Kamil Dzik <dzik-kamil@wp.pl>
2023-11-04 14:53:54 +0100

    add video carousel

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const nav = document.querySelector('.navbar')
-	const navList = document.querySelector('.navbar-collapse')
-	const toTheTopBtn = document.querySelector('.to-the-top')
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelector(".navbar");
+  const navList = document.querySelector(".navbar-collapse");
+  const toTheTopBtn = document.querySelector(".to-the-top");
 
-	document.onclick = function (e) {
-		if (e.target.id !== 'navbar') {
-			navList.classList.remove('show')
-		}
-	}
+  document.onclick = function (e) {
+    if (e.target.id !== "navbar") {
+      navList.classList.remove("show");
+    }
+  };
 
-	const hideBtn = () => {
-		if (window.scrollY > 300) {
-			toTheTopBtn.style.display = 'flex'
-			toTheTopBtn.classList.add('btnEntrance')
-		} else {
-			toTheTopBtn.style.display = 'none'
-		}
-	}
-	window.addEventListener('scroll', hideBtn)
-})
+  const hideBtn = () => {
+    if (window.scrollY > 300) {
+      toTheTopBtn.style.display = "flex";
+      toTheTopBtn.classList.add("btnEntrance");
+    } else {
+      toTheTopBtn.style.display = "none";
+    }
+  };
+  window.addEventListener("scroll", hideBtn);
+
+  const videos = document.querySelectorAll("video");
+
+  videos.forEach((video, index) => {
+    video.addEventListener("play", () => {
+      videos.forEach((otherVideo, otherIndex) => {
+        if (otherIndex !== index && !otherVideo.paused) {
+          otherVideo.pause();
+        }
+      });
+    });
+  });
+});
--- js/swiper.js
+++ js/swiper.js
@@ -1,32 +1,45 @@
-var swiper = new Swiper('.swiper', {
-	slidesPerView: 3,
-	spaceBetween: 0,
-	loop: true,
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	autoplay: {
-		delay: 2000,
-		disableOnInteraction: false,
-		pauseOnMouseEnter: true,
-	},
-	breakpoints: {
-		280: {
-			slidesPerView: 1,
-			spaceBetween: 20,
-		},
+const photosSwiper = new Swiper(".swiper.photos", {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
 
-		576: {
-			slidesPerView: 2,
-		},
+    576: {
+      slidesPerView: 2,
+    },
 
-		1200: {
-			slidesPerView: 3,
-		},
-	},
-})
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const videosSwiper = new Swiper(".swiper.videos", {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
